949e05bedf927deb76a824ab3513e1a2906149c1
houssem <hermessi.houssem@gmail.com>
2021-03-14 14:01:28 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Profile from "./Component/Profile/Profile";
+import UserList from "./Component/UserList/UserList";
+import { Route ,Switch} from "react-router-dom";
+
+function App() {
+  return (
+  <div className="App">
+      
+<Switch> 
+<Route exact path="/" render={() => <UserList />} />   
+<Route exact path="/user/:id" render={(props) => <Profile {...props} />} />
+</Switch>  
+
+  </div>
+  
+  );
+}
+
+export default App;
